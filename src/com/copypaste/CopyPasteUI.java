e68806b9f8d817fb54f4d5b0db5253fcb25a6a27
san4net <san4net@gmail.com>
2014-01-27 09:23:20 +0530

    options added

--- src/com/copypaste/CopyPasteUI.java
+++ src/com/copypaste/CopyPasteUI.java
@@ -5,15 +5,22 @@ import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowListener;
-import java.io.Serializable;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
+import javax.swing.BorderFactory;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.JRadioButton;
 import javax.swing.SpringLayout;
 import javax.swing.SwingUtilities;
 import javax.swing.WindowConstants;
@@ -21,92 +28,148 @@ import javax.swing.WindowConstants;
 import org.apache.commons.lang.StringUtils;
 
 import com.copypaste.logic.CopyLogic;
+import com.copypaste.util.Constants;
 import com.copypaste.util.Utils;
 
-public class CopyPasteUI<E> implements Serializable {
+public class CopyPasteUI<E> {
+
+	private static final String FILE_LOCATION = "c:/non_os/projects/serialize/cp.ser";
 	private CopyLogic copyLogic;
 	private JFrame frame = new JFrame("copyPaste");
-	private JLabel srcLabel = new JLabel("Copy : ", JLabel.TRAILING);
-	private JLabel destLabel = new JLabel("Paste : ");
+	private JLabel srcLabel;
+	private JLabel destLabel;
 	private JButton copyPasteButton = new JButton("Copy Paste");
-	private JTextField srcField = new JTextField(40);
-	private JTextField destField = new JTextField(40);
-
-	private JComboBox<E> srcCombo = new HistoricalCombo<>();
-	private JComboBox<E> destCombo = new HistoricalCombo<>();
+	private JComboBox<E> srcCombo;
+	private JComboBox<E> destCombo;
+	// radio button
+	private static ButtonGroup buttonGroup = new ButtonGroup();
+	private JRadioButton fileButton;
+	private JRadioButton directoryButton;
+	private JRadioButton directoryAndSubDirectoryButton;
 
 	private static Object[][] errCodeErrMsg = { { 0, "done" },
 			{ 1, "Pls enter proper url" }, { 2, "Entered file doesnot exist" } };
 
-	private WindowListener windowListener = Utils.getWindowListener();
+	private WindowListener windowListener = Utils.getWindowListener(this);
 
 	public void injectLogic(CopyLogic logic) {
 		this.copyLogic = logic;
 	}
+
 
 	private void createComponents() {
 		frame = new JFrame("copyPaste");
 		frame.addWindowListener(windowListener);
 		srcLabel = new JLabel("Copy : ", JLabel.TRAILING);
 		destLabel = new JLabel("Paste : ");
+
 		copyPasteButton = new JButton("Copy Paste");
 		copyPasteButton.addActionListener(listener);
 
-		srcField = new JTextField(40);
-		destField = new JTextField(40);
+		fileButton = new JRadioButton("file");
+		fileButton.setActionCommand(Constants.actionCommands[0]);
+		;
+		fileButton.setName("fileButton");
 
+		directoryButton = new JRadioButton("Directory");
+		directoryButton.setActionCommand(Constants.actionCommands[1]);
+		directoryButton.setName("directoryButton");
+
+		directoryAndSubDirectoryButton = new JRadioButton("directoryAndSubDir");
+		directoryAndSubDirectoryButton.setActionCommand(Constants.actionCommands[2]);
+		directoryAndSubDirectoryButton
+				.setName("directoryAndSubDirectoryButton");
+
+		buttonGroup.add(fileButton);
+		buttonGroup.add(directoryButton);
+		buttonGroup.add(directoryAndSubDirectoryButton);
+	}
+
+	private void putThingsTogetherForDisplay() {
+		Utils.checkNotNull(srcCombo);
+		Utils.checkNotNull(destCombo);
 		srcCombo.setSize(new Dimension(450, srcCombo.getPreferredSize().height));
 		destCombo.setSize(new Dimension(450,
 				destCombo.getPreferredSize().height));
 
 		SpringLayout layout = new SpringLayout();
 
-		JPanel panel = new JPanel(layout);
+		JPanel mainPanel = new JPanel(layout);
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.add(fileButton);
+		buttonPanel.add(directoryButton);
+		buttonPanel.add(directoryAndSubDirectoryButton);
+		buttonPanel.setBorder(BorderFactory
+				.createTitledBorder("Advance Option"));
+
 		Container contentPane = frame.getContentPane();
-		panel.add(srcLabel);
-		panel.add(srcCombo);
+		mainPanel.add(buttonPanel);
+		mainPanel.add(srcLabel);
+		mainPanel.add(srcCombo);
 
-		panel.add(destLabel);
-		panel.add(destCombo);
+		mainPanel.add(destLabel);
+		mainPanel.add(destCombo);
 
-		panel.add(copyPasteButton);
+		mainPanel.add(copyPasteButton);
+		int index = 5;
+		layout.putConstraint(SpringLayout.NORTH, buttonPanel, index,
+				SpringLayout.NORTH, contentPane);
+		layout.putConstraint(SpringLayout.WEST, buttonPanel, index,
+				SpringLayout.WEST, contentPane);
 
+		index = index + 5;
 		// Adjust constraints for the label so it's at (5,5).
-		layout.putConstraint(SpringLayout.WEST, srcLabel, 5, SpringLayout.WEST,
-				contentPane);
-		layout.putConstraint(SpringLayout.NORTH, srcLabel, 5,
-				SpringLayout.NORTH, contentPane);
+		layout.putConstraint(SpringLayout.NORTH, srcLabel, index,
+				SpringLayout.SOUTH, buttonPanel);
+		layout.putConstraint(SpringLayout.WEST, srcLabel, index,
+				SpringLayout.WEST, contentPane);
 
-		layout.putConstraint(SpringLayout.WEST, srcCombo, 5, SpringLayout.EAST,
-				srcLabel);
-		layout.putConstraint(SpringLayout.NORTH, srcCombo, 5,
-				SpringLayout.NORTH, contentPane);
+		layout.putConstraint(SpringLayout.NORTH, srcCombo, index,
+				SpringLayout.SOUTH, buttonPanel);
+		layout.putConstraint(SpringLayout.WEST, srcCombo, index,
+				SpringLayout.EAST, srcLabel);
 
-		layout.putConstraint(SpringLayout.WEST, destLabel, 5,
+		layout.putConstraint(SpringLayout.NORTH, destLabel, index,
+				SpringLayout.SOUTH, buttonPanel);
+		layout.putConstraint(SpringLayout.WEST, destLabel, index,
 				SpringLayout.EAST, srcCombo);
-		layout.putConstraint(SpringLayout.NORTH, destLabel, 5,
-				SpringLayout.NORTH, contentPane);
 
-		layout.putConstraint(SpringLayout.WEST, destCombo, 5,
+		layout.putConstraint(SpringLayout.NORTH, destCombo, index,
+				SpringLayout.SOUTH, buttonPanel);
+		layout.putConstraint(SpringLayout.WEST, destCombo, index,
 				SpringLayout.EAST, destLabel);
-		layout.putConstraint(SpringLayout.NORTH, destCombo, 5,
-				SpringLayout.NORTH, contentPane);
 
-		layout.putConstraint(SpringLayout.WEST, copyPasteButton, 5,
+		layout.putConstraint(SpringLayout.NORTH, copyPasteButton, index,
+				SpringLayout.SOUTH, buttonPanel);
+		layout.putConstraint(SpringLayout.WEST, copyPasteButton, index,
 				SpringLayout.EAST, destCombo);
-		layout.putConstraint(SpringLayout.NORTH, copyPasteButton, 5,
-				SpringLayout.NORTH, contentPane);
 
-		frame.add(panel);
+		frame.add(mainPanel);
 	}
-	
-   public void showGui(){
-	    createComponents();
-		frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-		frame.pack();
-		frame.setSize(new Dimension(1100, 100));
-		frame.setVisible(true);
-   }
+
+	private void initializeComponent() {
+		deserialize();
+	}
+
+	public void showGui() {
+		if (SwingUtilities.isEventDispatchThread()) {
+			createComponents();
+			initializeComponent();
+			putThingsTogetherForDisplay();
+			frame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+			frame.pack();
+			frame.setSize(new Dimension(1100, 300));
+			frame.setVisible(true);
+		} else {
+			SwingUtilities.invokeLater(new Runnable() {
+				@Override
+				public void run() {
+					showGui();
+				}
+			});
+		}
+	}
+
 	public static void main(String[] args) {
 		final CopyPasteUI<String> cp = new CopyPasteUI<>();
 		cp.injectLogic(new MemoryMappedCopyLogicImpl());
@@ -138,8 +201,9 @@ public class CopyPasteUI<E> implements Serializable {
 			}
 
 			Integer result = null;
+			
 			try {
-				result = copyLogic.startCopy(srcLoc, destLoc);
+				result = copyLogic.startCopy(srcLoc, destLoc,buttonGroup.getSelection().getActionCommand());
 			} catch (Exception e1) {
 				e1.printStackTrace();
 			}
@@ -148,4 +212,38 @@ public class CopyPasteUI<E> implements Serializable {
 			copyPasteButton.setEnabled(true);
 		}
 	};
+
+
+	public void serialize() throws IOException {
+		File f = new File(FILE_LOCATION);
+		if (!f.exists()) {
+			f.createNewFile();
+		}
+		try (FileOutputStream fOut = new FileOutputStream(f);
+				ObjectOutputStream objectOut = new ObjectOutputStream(fOut);) {
+			objectOut.writeObject(srcCombo);
+			objectOut.writeObject(destCombo);
+			System.out.println("object written");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
+	public void deserialize() {
+		File f = new File(FILE_LOCATION);
+		if (!f.exists())
+			return;
+
+		try (FileInputStream fIn = new FileInputStream(f);
+				ObjectInputStream objectOut = new ObjectInputStream(fIn);) {
+			srcCombo = (HistoricalCombo<E>) objectOut.readObject();
+			destCombo = (HistoricalCombo<E>) objectOut.readObject();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			srcCombo = new HistoricalCombo<>();
+			destCombo = new HistoricalCombo<>();
+		}
+	}
+
 }
--- src/com/copypaste/util/Utils.java
+++ src/com/copypaste/util/Utils.java
@@ -2,17 +2,36 @@ package com.copypaste.util;
 
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.IOException;
 
-public class Utils {
+import com.copypaste.CopyPasteUI;
 
-	public static WindowAdapter getWindowListener(){
-		return new MyWindowAdapter();
+public class Utils<T> {
+
+	public static <E> WindowAdapter getWindowListener(CopyPasteUI<E> comp){
+		return new MyWindowAdapter(comp);
 	}
 	
-	private static class MyWindowAdapter extends WindowAdapter{
+	private static class MyWindowAdapter<E> extends WindowAdapter{
+		CopyPasteUI<E> c;
+		public MyWindowAdapter(CopyPasteUI<E>comp) {
+			c = comp;
+		}
 		@Override
 		public void windowClosing(WindowEvent e) {
-			
+				try {
+					c.serialize();
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+		
 		}
 	}
+	
+  public static<T> T checkNotNull(T obj){
+	  if(obj == null) throw new NullPointerException();
+	  return obj;
+  }
+  
 }
